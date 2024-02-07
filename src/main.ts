7ddf7323414984001679a2c99a9b2822d22a18c0
Grzegorz Pokorski <mr.pokorski@gmail.com>
2024-02-07 12:08:13 +0100

    feat: accordion prototype

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import "./style.css";
+
+const accordions = document.querySelectorAll("details");
+
+accordions.forEach((accordion) => {
+  const summary = accordion.querySelector("summary");
+  if (!summary) {
+    throw Error("details should contain summary");
+  }
+  const indicator = summary.querySelector("[data-indicator]");
+
+  summary.addEventListener("click", () => {
+    if (indicator && accordion.open) {
+      indicator.innerHTML = "+";
+    }
+    if (indicator && !accordion.open) {
+      indicator.innerHTML = "-";
+    }
+  });
+});
